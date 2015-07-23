b987a208da2fb36af6573d1069cfc5e47521de7e
Kotaro Hara <koe.bluebear@gmail.com>
2015-07-23 12:08:55 -0400

    Updated Sidewalk.py

--- ToSidewalk/db/Sidewalk.py
+++ ToSidewalk/db/Sidewalk.py
@@ -1,5 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, TIMESTAMP
 from geoalchemy2 import Geometry
+from geoalchemy2.shape import *
+from shapely.geometry import LineString
+import json
+import numpy as np
+import os
+
 
 import db
 
@@ -20,6 +26,49 @@ class SidewalkEdgeTable(db.Base):
     deleted = Column(Boolean, name="deleted")
     timestamp = Column(TIMESTAMP, name="timestamp")
 
+
+def example_insert(self):
+    """
+    Example
+
+    http://geoalchemy-2.readthedocs.org/en/latest/core_tutorial.html
+
+    :return:
+    """
+    filename = os.path.relpath("../../resources", os.path.dirname(__file__)) + "/SmallMap_04_Sidewalks.geojson"
+    with open(filename) as f:
+        geojson = json.loads(f.read())
+
+    table = SidewalkEdgeTable().__table__
+    conn = self.engine.connect()
+    for feature in geojson["features"]:
+        # print pp.pprint(feature)
+        coordinates = feature["geometry"]["coordinates"]
+        properties = feature["properties"]
+
+        geom = from_shape(LineString(coordinates), srid=4326)
+        sidewalk_edge_id = int(np.uint32(properties["sidewalk_edge_id"]))  # [Issue X] I've just realized functions in pgrouting assume 32bit int, which kind of sucks. Hacking it for now with np.uint32
+        x1 = float(properties["x1"])
+        y1 = float(properties["y1"])
+        x2 = float(properties["x2"])
+        y2 = float(properties["y2"])
+        way_type = properties["type"]
+        source = int(np.uint32(properties["source"]))
+        target = int(np.uint32(properties["target"]))
+
+        ins = table.insert().values(geom=geom,
+                                    sidewalk_edge_id=sidewalk_edge_id,
+                                    x1=x1,
+                                    y1=y1,
+                                    x2=x2,
+                                    y2=y2,
+                                    way_type=way_type,
+                                    source=source,
+                                    target=target,
+                                    deleted=False,
+                                    parent_sidewalk_edge_id=None)
+
+        conn.execute(ins)
 
 if __name__ == "__main__":
     database = db.DB("../../.settings")
