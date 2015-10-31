adf9c282b86bc7aa86d10cc2f773f19f08aad5dd
Kotaro Hara <koe.bluebear@gmail.com>
2015-10-30 22:09:25 -0400

    Enabled mutiprocessing
    Former-commit-id: a9e120be6d99fe858b5419b4efa4f7211f6bcbc2

--- ToSidewalk/example/parse_osm.py
+++ ToSidewalk/example/parse_osm.py
@@ -1,13 +1,17 @@
+"""
+This scripts read a osm file, parse street segments, and export the parsed data in osm format.
+"""
 from ToSidewalk.graph import *
 
 
-def main(filename):
-    geometric_graph = parse_osm(filename)
-    geometric_graph = clean_edge_segmentation(geometric_graph)
-    geometric_graph = split_path(geometric_graph)
-    geometric_graph = remove_short_edges(geometric_graph)
-    with open("../../output/output.osm", "wb") as f:
+def main(in_filename, out_filename):
+    geometric_graph = parse_osm(in_filename)
+    # geometric_graph = clean_edge_segmentation(geometric_graph)
+    # geometric_graph = split_path(geometric_graph)
+    # geometric_graph = remove_short_edges(geometric_graph)
+    with open(out_filename, "wb") as f:
         f.write(geometric_graph.export(format="osm"))
 
 if __name__ == "__main__":
-    main("../../resources/SmallMap_04.osm")
+    main("../../resources/DC_IntersectedWithTheCityBoundary/district-of-columbia-latest.osm",
+         "../../output/dc-streets-from-district-of-columbia-latest.osm")
