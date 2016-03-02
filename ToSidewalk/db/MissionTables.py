0ae0f0efeb330f96c38eed655a02e4458e42341b
Kotaro Hara <koe.bluebear@gmail.com>
2016-03-02 14:42:24 -0500

    Updated table schema

--- ToSidewalk/db/MissionTables.py
+++ ToSidewalk/db/MissionTables.py
@@ -7,14 +7,15 @@ class MissionTable(db.Base):
     __table_args__ = {'schema': 'sidewalk'}
     mission_id = Column(Integer, primary_key=True, name="mission_id", autoincrement=True)
     region_id = Column(Integer, name="region_id")
-    mission = Column(String, name="mission")
-    level = Column(Float, name="level")
-    # description = Column(String, name="description")
+    label = Column(String, name="label")
+    level = Column(Integer, name="level")
+    distance = Column(Float, name="distance")
+    coverage = Column(Float, name="coverage")
     deleted = Column(Boolean, name="deleted", default=False)
 
     def __repr__(self):
-        return "Mission(mission_id=%s, region_id=%s, mission=%s, level=%s, description=%s)" % \
-               (self.mission_id, self.region_id, self.mission, self.level, self.description)
+        return "Mission(mission_id=%s, region_id=%s, label=%s, level=%s, distance=%s, coverage=%s)" % \
+               (self.mission_id, self.region_id, self.label, self.level, self.distance, self.coverage)
 
     @classmethod
     def list(cls, session):
@@ -25,6 +26,18 @@ class MissionTable(db.Base):
         :return:
         """
         query = session.query(cls)
+        return [record for record in query]
+
+    @classmethod
+    def list_missions_with_label(cls, session, label):
+        """
+        Returns a list of missions with the given label
+
+        :param session:
+        :param label:
+        :return:
+        """
+        query = session.query(cls).filter(cls.label == label)
         return [record for record in query]
 
     @classmethod
@@ -59,5 +72,5 @@ if __name__ == "__main__":
     database = db.DB("../../.settings")
     session = database.session
 
-    for mission in MissionTable.list(session):
+    for mission in MissionTable.list_missions_with_label(session, "distance-mission"):
         print mission
--- ToSidewalk/scripts/populate_missions.py
+++ ToSidewalk/scripts/populate_missions.py
@@ -7,43 +7,93 @@ def populate_missions(session):
     mission_types = {
         "initial-mission": {
             "id": "initial-mission",
-            "levels": [1]
+            "levels": [1],
+            "distances": [250],  # meters (m)
+            "coverages": [None]
         },
-        "neighborhood-coverage-challenge": {
-            "id": "neighborhood-coverage-challenge",
-            "levels": [1, 2, 3]
+        "distance-mission": {
+            "id": "distance-mission",
+            "levels": [1, 2, 3, 4, 5, 6, 7],
+            "distances": [500, 1000, 2500, 5000, 10000, 15000, 20000],  # meters (m)
+            "coverages": [None, None, None, None, None, None, None]
         },
-        "distance-challenge": {
-            "id": "distance-challenge",
-            "levels": [0.5, 1, 2.5, 5, 10, 15, 20]  # km
-        },
-        "area-coverage-challenge": {
-            "id": "area-coverage-challenge",
-            "levels": [25, 50, 75, 100]  # %
+        "area-coverage-mission": {
+            "id": "area-coverage-mission",
+            "levels": [1, 2, 3, 4],
+            "distances": [None, None, None, None],
+            "coverages": [25, 50, 75, 100]  # percentages (%)
         }
     }
 
     # Populate missions
     missions = []
-    missions.append(MissionTable(mission="initial-mission", level=1))
-    missions.append(MissionTable(mission="neighborhood-coverage-challenge", level=1))
-    missions.append(MissionTable(mission="neighborhood-coverage-challenge", level=2))
-    missions.append(MissionTable(mission="neighborhood-coverage-challenge", level=3))
+    missions.append(MissionTable(label="initial-mission", level=1, distance=250))
     for record in RegionTable.list_region_of_type(session, "neighborhood"):
-        for level in mission_types["distance-challenge"]["levels"]:
-            mission = MissionTable(region_id=record.region_id, mission="distance-challenge", level=level)
+        for level, distance, coverage in zip(mission_types["distance-mission"]["levels"], mission_types["distance-mission"]["distances"], mission_types["distance-mission"]["coverages"]):
+            mission = MissionTable(region_id=record.region_id, label="distance-mission", level=level, distance=distance, coverage=coverage)
             missions.append(mission)
 
-        for level in mission_types["area-coverage-challenge"]["levels"]:
-            mission = MissionTable(region_id=record.region_id, mission="area-coverage-challenge", level=level)
+        for level, distance, coverage in zip(mission_types["area-coverage-mission"]["levels"], mission_types["area-coverage-mission"]["distances"], mission_types["area-coverage-mission"]["coverages"]):
+            mission = MissionTable(region_id=record.region_id, label="area-coverage-mission", level=level, distance=distance, coverage=coverage)
             missions.append(mission)
     MissionTable.add_missions(session, missions)
 
+
+def compute_area_coverage_from_distance(session):
+    """
+
+    :param session:
+    :return:
+    """
+    total_distance_by_region = {}
+
+    distance_query = """SELECT SUM(ST_Length(ST_Transform(street_edge.geom, 26985))) FROM sidewalk.region
+    INNER JOIN sidewalk.street_edge
+    ON region.geom && street_edge.geom
+    WHERE region.region_id = %s"""
+
+    region_ids = set(map(lambda m: m.region_id, filter(lambda m: m.region_id is not None, MissionTable.list(session))))
+    for region_id in region_ids:
+        total_distance_by_region[region_id] = session.execute(distance_query % str(region_id)).fetchone()[0]
+
+    # from pprint import pprint
+    # pprint(total_distance_by_region)
+
+    for mission in MissionTable.list_missions_with_label(session, "distance-mission"):
+        total_distance = total_distance_by_region[mission.region_id]
+        coverage = round(mission.distance / total_distance * 100, 2)
+
+        mission.coverage = coverage
+        if coverage >= 100:
+            mission.deleted = True
+    session.commit()
+
+
+def compute_distance_from_area_coverage(session):
+    total_distance_by_region = {}
+
+    distance_query = """SELECT SUM(ST_Length(ST_Transform(street_edge.geom, 26985))) FROM sidewalk.region
+    INNER JOIN sidewalk.street_edge
+    ON region.geom && street_edge.geom
+    WHERE region.region_id = %s"""
+
+    region_ids = set(map(lambda m: m.region_id, filter(lambda m: m.region_id is not None, MissionTable.list(session))))
+    for region_id in region_ids:
+        total_distance_by_region[region_id] = session.execute(distance_query % str(region_id)).fetchone()[0]
+
+    for mission in MissionTable.list_missions_with_label(session, "area-coverage-mission"):
+        total_distance = total_distance_by_region[mission.region_id]
+        distance = round(total_distance * mission.coverage / 100, 2)
+        mission.distance = distance
+    session.commit()
 
 if __name__ == "__main__":
     print("MissionTables.py")
     database = DB("../../.settings")
     session = database.session
-    populate_missions(session)
+    # populate_missions(session)
+    # compute_area_coverage_from_distance(session)
+    compute_distance_from_area_coverage(session)
+
 
 
